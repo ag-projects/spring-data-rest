04abaa100c013d492541d7b0fe4c8ce122f2c48f
Armen G <gharibi.armen@yahoo.com>
2018-01-06 13:11:08 -0800

    Added authorization to manufacturer repository

--- src/main/java/com/agharibi/springdatarest/repository/ManufacturerJpaRepository.java
+++ src/main/java/com/agharibi/springdatarest/repository/ManufacturerJpaRepository.java
@@ -1,8 +1,12 @@
 package com.agharibi.springdatarest.repository;
 
 import com.agharibi.springdatarest.models.Manufacturer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -10,9 +14,8 @@ import java.util.List;
 
 @Repository
 @RepositoryRestResource(path = "manu", collectionResourceRel = "manu")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public interface ManufacturerJpaRepository extends JpaRepository<Manufacturer, Long> {
-
-
 
     List<Manufacturer> findByFoundedDateBefore(Date date);
 
